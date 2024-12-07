d7fc50a95bf80c3e3115f023a3051001441ebd45
Just_NoWorking322 <alisherbolotbekov03@gmail.com>
2024-12-07 20:06:27 +0600

    base info

---
+++ bot.py
@@ -0,0 +1,17 @@
+import random
+from aiogram import Bot, Dispatcher, types
+import asyncio
+import logging
+
+TOKEN = "YOUR_BOT_TOKEN"
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def on_start():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(on_start())
